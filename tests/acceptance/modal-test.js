bd6469ba8a0fbc37cd21f184eb0ef997d736bd7a
Blake Gentry <blakesgentry@gmail.com>
2018-07-30 10:26:34 -0700

    add a working modal acceptance test

---
+++ tests/acceptance/modal-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { click, find, visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | modal', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('opening a modal', async function(assert) {
+    let done = assert.async();
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    await click(".open-modal-button")
+    assert.ok(find(".modal-footer button.btn"), "it shows the modal");
+
+    done();
+  });
+});
